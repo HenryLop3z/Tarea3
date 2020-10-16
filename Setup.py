4a3e69c20afeed3d1a505f083c69b1967d7d47fc
HenryLop3z <henrylopez@estudiantec.cr>
2020-10-16 03:23:14 -0600

    Archivos Tarea 3

--- Setup.py
+++ Setup.py
@@ -7,22 +7,22 @@ here = pathlib.Path(__file__).parent.resolve()
 
 
 setup(
-    # Con esta linea se instala el programa
+    # Con esta linea se instala el paquete
     # $ pip3 install git+https://github.com/HenryLop3z/Tarea3
 
     name='Tarea3DonatoLeeLopezRamirez',  # Nombre del proyecto
     version='1.0.0',  # Versión del programa
-    description='Paquete para la tarea 3',
+    description='Paquete para la tarea 3',  # Descripción breve
     url='git@github.com:HenryLop3z/Tarea3.git',  # Enlace al proyecto
-    author='DonatoLeeLopezRamirez',
+    author='DonatoLeeLopezRamirez',  # Autor
     author_email='henrylopez@estudiantec.cr',
     license='unlicense',
-    package_dir={'Tarea3DonatoLeeLopezRamirez': 'T3_pkg'},  # especifica el directorio de los paquetes
+    package_dir={'T3_pkg': 'T3_pkg'},  # especifica el directorio de los paquetes
     packages=['T3_pkg'],  # Encuentra los paquetes necesarios
     install_requires=['Pillow', 'tabulate', 'playsound', 'argparse'],  # Instala las librerias necesarias
     python_requires='>=3.3',  # Versión de Python compatible
-    package_data={  # Archivos
+    package_data={  # Archivos extras a los paquetes
         'T3_pkg': ['meca.jpg', 'Texto_ejemplo.txt', 'Hello.mp3'],
     },
-    scripts=['T3_pkg/lector_texto.py', 'T3_pkg/presentador_de_audio.py', 'T3_pkg/Presentador_de_imágenes.py']
+    scripts=['T3_pkg/lector_texto.py', 'T3_pkg/presentador_de_audio.py', 'T3_pkg/Presentador_de_imágenes.py'] 
 )
